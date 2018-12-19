6f93be8fd69d959a8d841054469868f12d97e3b5
David Díaz González <dgonzalez@suse.de>
2018-12-19 08:30:59 +0000

    Rename `source` named param to `handler`

--- library/packages/src/lib/y2packager/product_license.rb
+++ library/packages/src/lib/y2packager/product_license.rb
@@ -49,15 +49,16 @@ module Y2Packager
       #
       # This method uses a cache to return an already fetched product license.
       #
-      # @param product_name [String]       Product's name
-      # @param source       [:libzypp,nil] Source to get the license from. For the time being,
-      #   only :libzypp is really supported.
+      # @param product_name [String] Product's name
+      # @param handler      [:libzypp, nil] Handler for interact with license acceptance. For the
+      #   time being, only :libzypp is really supported.
+      #
       # @return [ProductLicense]
-      def find(product_name, source: nil, content: nil)
+      def find(product_name, handler: nil, content: nil)
         return cache[product_name] if cache[product_name]
-        license = License.find(product_name, source: source, content: content)
+        license = License.find(product_name, content: content)
         return nil unless license
-        cache[product_name] = ProductLicense.new(product_name, license, source: source)
+        cache[product_name] = ProductLicense.new(product_name, license, handler: handler)
       end
 
       # Clear product licenses cache
@@ -73,12 +74,12 @@ module Y2Packager
     # Constructor
     #
     # @param product_name [String] Product name to get licenses for
-    # @param source       [Symbol] Backend to use when syncing the licenses acceptance status
+    # @param handler       [Symbol] Backend to use when syncing the licenses acceptance status
     #   (only :libzypp is supported)
-    def initialize(product_name, license, source: nil)
+    def initialize(product_name, license, handler: nil)
       @product_name = product_name
       @license = license
-      @handler = Y2Packager::LicensesHandlers.for(source, product_name) if source
+      @handler = Y2Packager::LicensesHandlers.for(handler, product_name) if handler
     end
 
     # Determine whether the license have been accepted or not
@@ -91,7 +92,7 @@ module Y2Packager
 
     # Accept the license
     #
-    # As a side effect, it will update the source acceptance
+    # As a side effect, it will update the license acceptance
     def accept!
       license.accept!
       sync_acceptance
